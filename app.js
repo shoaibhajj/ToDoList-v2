9331260ccd76065f16850b0eaa40d8c157c979dd
shoaibhajj <44305345+shoaibhajj@users.noreply.github.com>
2023-06-07 13:27:40 +0300

    Update app.js
    change port

--- app.js
+++ app.js
@@ -137,6 +137,8 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port=process.env.PORT ||3000;
+app.listen(port, function() {
+  console.log(`Server started on port ${port}`);
 });
+
